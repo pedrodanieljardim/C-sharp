8b1d791b1590454fb1d234c3289dcba2c1ab4e85
pedrodanieljardim <pedrodanieljardim@gmail.com>
2020-04-11 13:03:54 -0300

    programa olá mundo comentado

---
+++ Acenelio/Introducao a .NET e C#/helloworldcomentado.cs
@@ -0,0 +1,16 @@
+using System;
+// referência a biblioteca padrão do DOTnet que possui funções básicas para o programa..
+namespace ConsoleApp1
+    // namespace geralmente é o aonde foi nomeada a pasta gerada pelo VS ide.
+{
+    class Program
+        // declaracao de classe 
+    {
+        static void Main(string[] args)
+            // método main (entry point) do C#
+        {
+            Console.WriteLine("Hello World!");
+            // função de print na tela....
+        }
+    }
+}
