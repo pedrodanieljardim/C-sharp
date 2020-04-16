85ab9d4d3ff9a19d6c4de86ff480e8682e296c53
pedrodanieljardim <pedrodanieljardim@gmail.com>
2020-04-16 20:46:11 -0300

    Operadores de Atribuição

---
+++ Acenelio/Fundamentos de Programação em C#/Operadores de atribuição/operaAtribui.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Globalization;
+
+namespace FundamentosProgramacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+            a += 2;
+            Console.WriteLine(a);
+            a *= 3;
+            Console.WriteLine(a);
+            string s = "ABC";
+            Console.WriteLine(s);
+            s += "DEF";
+            Console.WriteLine(s);
+        }
+    }
+}
