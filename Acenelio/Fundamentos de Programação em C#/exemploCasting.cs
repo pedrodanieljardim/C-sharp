143dc0d1469bdd618f6e142e7782fe5662c7b14c
pedrodanieljardim <pedrodanieljardim@gmail.com>
2020-04-16 20:03:44 -0300

    Exemplo 1 de casting

---
+++ Acenelio/Fundamentos de Programação em C#/exemploCasting.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace FundamentosProgramacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            float b;
+            a = 5.1;
+            b = (float)a;
+            Console.WriteLine(b);
+        }
+    }
+}
