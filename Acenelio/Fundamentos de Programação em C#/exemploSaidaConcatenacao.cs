3f13b1a9c7b105b1ad1504c239f9223b3e6c1c57
pedrodanieljardim <pedrodanieljardim@gmail.com>
2020-04-16 20:11:58 -0300

    Saida de dados por Concatenacao

---
+++ Acenelio/Fundamentos de Programação em C#/exemploSaidaConcatenacao.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace FundamentosProgramacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 32;
+            double saldo = 10.35784;
+            String nome = "Maria";
+            Console.Write("Bom dia!");
+            Console.WriteLine("Boa tarde!");
+            Console.WriteLine("Boa noite!");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+        }
+    }
+}
