4e97691e0bc6cfb5af1ea94bb82d4c96fc9eab95
pedrodanieljardim <pedrodanieljardim@gmail.com>
2020-04-16 20:05:40 -0300

    exemplo 3 de casting

---
+++ Acenelio/Fundamentos de Programação em C#/exemploCasting3.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace FundamentosProgramacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 2;
+            double resultado = (double)a / b;
+            Console.WriteLine(resultado);
+        }
+    }
+}
