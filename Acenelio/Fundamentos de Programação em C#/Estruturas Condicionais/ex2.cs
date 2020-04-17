7af3eb00c738c4c1927ee72cceaca71d27fd9eaf
pedrodanieljardim <pedrodanieljardim@gmail.com>
2020-04-17 18:53:34 -0300

    Ex 2 sobre estruturas condicionais

---
+++ Acenelio/Fundamentos de Programação em C#/Estruturas Condicionais/ex2.cs
@@ -0,0 +1,24 @@
+using System;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a hora atual?");
+            int hora = int.Parse(Console.ReadLine());
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (hora < 18)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite!");
+            }
+        }
+    }
+}
