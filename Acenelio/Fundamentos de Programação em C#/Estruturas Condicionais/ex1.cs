3fbbf04bda000d400590b2e38294e76836e93c79
pedrodanieljardim <pedrodanieljardim@gmail.com>
2020-04-17 18:52:11 -0300

    Exemplo de estruturas condicionais

---
+++ Acenelio/Fundamentos de Programação em C#/Estruturas Condicionais/ex1.cs
@@ -0,0 +1,20 @@
+using System;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com um número inteiro:");
+            int x = int.Parse(Console.ReadLine());
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Par!");
+            }
+            else
+            {
+                Console.WriteLine("Impar");
+            }
+        }
+    }
+}
